84b8c466e851a96c6c8f65f89cca2c537a40a4cb
tkkiethotuan <97288041+tkkiethotuan@users.noreply.github.com>
2022-09-15 21:56:25 +0700

    update

---
+++ assets/js/movie.js
@@ -0,0 +1,309 @@
+function to_dark_theme()
+{  
+    var body_theme = document.querySelector("body")
+    body_theme.setAttribute("class", "text-bg-dark")
+    var button = document.querySelector("#change")
+    button.innerText = "toggle_on"
+    button.setAttribute("onclick", "to_light_theme()")
+}
+
+function to_light_theme()
+{  
+    var body_theme = document.querySelector("body")
+    body_theme.setAttribute("class", "text-bg-light")
+    var button = document.querySelector("#change")
+    button.innerText = "toggle_off"
+    button.setAttribute("onclick", "to_dark_theme()")
+}
+
+// anime
+
+// darling in the franxx
+
+function darling_in_the_franxx_p1_t1()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/8485ae387a981d783f8764e508151cd9')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t1__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 1"
+}
+
+function darling_in_the_franxx_p1_t2()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/0a348ede8ac3768875037baca5de6e26')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t2__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 2"
+}
+
+function darling_in_the_franxx_p1_t3()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/7535bbb91c8fde347ad861f293126633')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t3__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 3"
+}
+
+function darling_in_the_franxx_p1_t4()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/fd9dcf1d146272bb05c490d790b6da52')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t4__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 4"
+}
+
+function darling_in_the_franxx_p1_t5()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/3ea2db50e62ceefceaf70a9d9a56a6f4')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t5__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 5"
+}
+
+function darling_in_the_franxx_p1_t6()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/e2065cb56f5533494522c46a72f1dfb0')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t6__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 6"
+}
+
+function darling_in_the_franxx_p1_t7()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/d494020ff8ec181ef98ed97ac3f25453')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t7__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 7"
+}
+
+function darling_in_the_franxx_p1_t8()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/d2350a54d774001d6078e326b4488878')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t8__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 8"
+}
+
+function darling_in_the_franxx_p1_t9()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/87f7ee4fdb57bdfd52179947211b7ebb')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t9__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 9"
+}
+
+function darling_in_the_franxx_p1_t10()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/73231e53eeef362c814c8522f5257286')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t10__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 10"
+}
+
+function darling_in_the_franxx_p1_t11()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/2a3d6d6cc4b5e77238c1fc1bb6cdd681')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t11__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 11"
+}
+
+function darling_in_the_franxx_p1_t12()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/e0126439e08ddfbdf4faa952dc910590')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t12__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 12"
+}
+
+function darling_in_the_franxx_p1_t13()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/0e7c7d6c41c76b9ee6445ae01cc0181d')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t13__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 13"
+}
+
+function darling_in_the_franxx_p1_t14()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/7d2be41b1bde6ff8fe45150c37488ebb')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t14__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 14"
+}
+
+function darling_in_the_franxx_p1_t15()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/0141a8aedb1b53970fac7c81dac79fbe')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t15__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 15"
+}
+
+function darling_in_the_franxx_p1_t16()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/15e122e839dfdaa7ce969536f94aecf6')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t16__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 16"
+}
+
+function darling_in_the_franxx_p1_t17()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/9a85c12a21b76392747906fc7b2aff92')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t17__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 17"
+}
+
+function darling_in_the_franxx_p1_t18()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/0dbb3fb9a5cd1d5f8a9075b5bb8070aa')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t18__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 18"
+}
+
+function darling_in_the_franxx_p1_t19()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/0ffaca95e3e5242ba1097ad8a9a6e95d')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t19__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 19"
+}
+
+function darling_in_the_franxx_p1_t20()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/7288251b27c8f0e73f4d7f483b06a785')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t20__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 20"
+}
+
+function darling_in_the_franxx_p1_t21()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/5fef3eff51dc719c4a9f565a742d78f2')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t21__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 21"
+}
+
+function darling_in_the_franxx_p1_t22()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/cf1cf43cba274ae7f413e864682b80f8')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t22__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 22"
+}
+
+function darling_in_the_franxx_p1_t23()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/b5b0db7f3a77ca4fcf9eca57aa7181ca')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t23__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 23"
+}
+
+function darling_in_the_franxx_p1_t24()
+{
+    var tap = document.querySelector("#darling_in_the_franxx")
+    tap.setAttribute('src', 'https://hd.1080phim.com/share/1fd7b875d3473501d1b771ebe934ca59')
+
+    var button = document.querySelector("#darling_in_the_franxx_p1_t24__movie")
+    button.setAttribute("class", "btn btn-primary d-inline-block active")
+
+    var movieName = document.querySelector("#movie_name")
+    movieName.innerText = "Darling In The Franxx tập 24"
+}
